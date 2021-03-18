ca4fcf1c9d23e76dd676778e61db77906b5b931d
jessicaUP <uphoff.jessica@gmail.com>
2021-03-18 15:43:44 -0400

    Made specs for Array#to_sum and my_uniq

--- lib/TDD.rb
+++ lib/TDD.rb
@@ -1,5 +1,21 @@
 def my_uniq(arr)
-    new_array = []
-    arr.each { |el| new_array << el if !new_array.include?(el)}
-    new_array
+    # new_array = []
+    # arr.each { |el| new_array << el if !new_array.include?(el)}
+    # new_array
+    arr.uniq
+end
+
+
+class Array
+    def to_sum
+        new_arr = []
+
+        (0...self.length).each do |i|
+            (i+1...self.length).each do |j|
+                new_arr << [i,j] if self[i] + self[j] == 0
+            end
+        end
+        new_arr
+    end
+
 end
--- spec/TDD_spec.rb
+++ spec/TDD_spec.rb
@@ -9,19 +9,44 @@ describe "my_uniq" do
   
     it "should return a new array" do
         arr = [1, 2, 3, 2, 4]
-        expect (my_uniq(arr).object_id).to_not eq(arr.object_id)
+        expect(my_uniq(arr)).to_not be(arr)
     end
 
     it "should not have any duplicate" do
         arr = [1, 2, 3, 2, 4]
         new_arr = arr.uniq
-        expect (my_uniq(arr)).to eq(new_arr)
+        expect(my_uniq(arr)).to eq(new_arr)
     end
 
     it "should not call .uniq method" do
         arr = [1, 2, 3, 2, 4]
-        expect (my_uniq(arr)).to_not receive(:uniq)
+        expect(my_uniq(arr)).to_not receive(:uniq)
     end 
+
+    it "accepting an array as an arg" do
+        expect { my_uniq([1, 2, 3, 2, 4]) }.to_not raise_error
+    end
 end
+
+describe Array do
+        # [-1, 0, 2, -2, 1].two_sum # => [[0, 4], [2, 3]]
+        # there are no matches, return empty array
+    describe "to_sum" do
+        it "find matching 0 sum pair indices" do 
+            arr = [-1, 0, 2, -2, 1]
+            expect(arr.to_sum).to eq([[0, 4], [2, 3]])
+        end
+
+        it "there are no matches, should return empty array" do 
+            arr = [1, 1, 0]
+            expect(arr.to_sum).to eq([])
+        end
+
+    end
+
+
+end
+
+
 
 
